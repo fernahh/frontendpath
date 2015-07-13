0f398a0b83d341de177ddf6f282173e24aa388f1
Luiz Fernando Rodrigues <fernahh@gmail.com>
2015-07-13 12:35:17 -0300

    Add lib to smooth scroll

--- source/assets/javascripts/application.js
+++ source/assets/javascripts/application.js
@@ -1,5 +1,7 @@
+//= require 'smooth-scroll.js/dist/js/smooth-scroll.min'
 //= require 'lib/page_scroll_indicator'
 
 window.onload = function() {
   PageScrollIndicator.createProgressBar("guide-article", "guide-content");
+  smoothScroll.init();
 }
